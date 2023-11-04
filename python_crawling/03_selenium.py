462f081f4a8d3a906f28634c3048abaee48ca674
poma0917 <a01093333894@gmail.com>
2023-11-04 12:31:56 +0900

    11월 4일 네이버 웹툰

---
+++ python_crawling/03_selenium.py
@@ -0,0 +1,25 @@
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import lxml
+import requests
+from bs4 import BeautifulSoup
+
+browser = webdriver.Chrome(ChromeDriverManager().install())
+url = "https://comic.naver.com/webtoon?tab=sat"
+
+browser.get(url)
+time.sleep(1)
+
+soup = BeautifulSoup(browser.page_source, "lxml")
+
+top3 = soup.find("ul", attrs = {"class" : "TripleRecommendList__triple_recommend_list--vm8_k"})
+title = top3.findAll("span", attrs = {"class" : "ContentTitle__title--e3qXt"})
+author = top3.findAll("a", attrs = {"class" : "ContentAuthor__author--CTAAP"})
+rate = top3.findAll("span", attrs = {"class" : "Rating__star_area--dFzsb"})
+
+print("-----토요웹툰 추천3개-----")
+for i in range(len(title)):
+  print(f"{i+1} - {title[1].text} || {author[1].text} || {rate[1].text}")
