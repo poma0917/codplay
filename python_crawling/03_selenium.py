4c3009c9be639664dd60ed11e6dc7b4a50eb707d
poma0917 <a01093333894gmail.com>
2023-11-11 12:35:20 +0900

    네이버 해킹 2일차

--- python_crawling/03_selenium.py
+++ python_crawling/03_selenium.py
@@ -7,7 +7,7 @@ import lxml
 import requests
 from bs4 import BeautifulSoup
 
-browser = webdriver.Chrome(ChromeDriverManager().install())
+browser = webdriver.Chrome()
 url = "https://comic.naver.com/webtoon?tab=sat"
 
 browser.get(url)
@@ -15,11 +15,12 @@ time.sleep(1)
 
 soup = BeautifulSoup(browser.page_source, "lxml")
 
-top3 = soup.find("ul", attrs = {"class" : "TripleRecommendList__triple_recommend_list--vm8_k"})
+top3 = soup.find("ul", attrs = {"class" : "AsideList__content_list--FXDvm"})
 title = top3.findAll("span", attrs = {"class" : "ContentTitle__title--e3qXt"})
 author = top3.findAll("a", attrs = {"class" : "ContentAuthor__author--CTAAP"})
-rate = top3.findAll("span", attrs = {"class" : "Rating__star_area--dFzsb"})
+# rate = top3.findAll("span", attrs = {"class" : "Rating__star_area--dFzsb"})
 
-print("-----토요웹툰 추천3개-----")
+print("-----실시간 인기 웹툰 5-----")
 for i in range(len(title)):
-  print(f"{i+1} - {title[1].text} || {author[1].text} || {rate[1].text}")
+  print(f"{i+1} - {title[i].text} || {author[i].text}")
+# || {rate[1].text}")
