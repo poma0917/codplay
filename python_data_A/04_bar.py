a5e3e2d8262066364a8620ba40a8143fbaf7a287
poma0917 <a01093333894@gmail.com>
2024-12-07 12:32:20 +0900

    고등 조지기

---
+++ python_data_A/04_bar.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import csv
+
+f = open('python_data_A/CSV_data/yp_h.csv', 'r', encoding='utf8')
+data = csv.reader(f)
+# result = []
+yp_all = []
+label_ages = ["0~9","10~19","20~29","30~39","40~49","50~59","60~69","70~79","80~89","90~99","100"]
+label_gen = ["청소년(0~19)","청년(2~39)","장년(4~59)","노년(60~)"]
+yp_gen = []
+
+for i in data:
+    yp_all.append(i)
+
+for da in range(len(yp_all)):
+    for change in range(len(yp_all[da][3:])):
+        if "," in yp_all[da][change + 3]:
+            yp_all[da][change + 3] = int(yp_all[da][change + 3].replace(",", ""))
+        else:
+            yp_all[da][change + 3] = int(yp_all[da][change + 3])
+
+yp_gen.append(sum(yp_all[0][3:5]))
+yp_gen.append(sum(yp_all[0][5:7]))
+yp_gen.append(sum(yp_all[0][7:9]))
+yp_gen.append(sum(yp_all[0][9:]))
+
+plt.pie(yp_gen, labels=label_gen, autopct="%.1f%%")
+# plt.pie(yp_all[10][3:], labels=label_ages, autopct="%.1f%%")
+plt.show()
+
+
+# result = []
+
+# for row in data :
+#     if '양평읍' in row[0]:
+#         for i in row[3:]:
+#             print(i)
+#             if ',' in i:
+#                 i = i.replace(',', '')
+#             result.append(int(i))
+
+# plt.bar(range(len(result)), result)
+# # plt.barh(range(len(result)), result)
+# plt.show()
